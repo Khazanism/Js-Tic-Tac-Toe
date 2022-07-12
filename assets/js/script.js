c3f04e61f24ce0b55ea4c742ea8f32243515177a
Khazanins <khazanyassin@gmail.com>
2022-07-12 23:06:00 +0200

    JS - Place Mark logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,42 @@
 //  Tic Tac Toe
 
+const X_CLASS = 'x';
+const CIRCLE_CLASS = 'circle';
+
+const cellElements = document.querySelectorAll("[data-cell]");
+let circleTurn;
+
+cellElements.forEach((cell) => {
+  cell.addEventListener("click", handleClick, { once: true });
+});
+
+// function handleClick(e) {
+//   // place mark
+
+//   const cell = e.target;
+//   const currentClass = circleTurn ? CIRCLE_CLASS : x_CLASS;
+//   placeMark(cell, currentClass);
+
+//   // check for win
+//   //check for draw
+//   //switch turns
+// }
+
+function handleClick(e) {
+  // place mark
+  const cell = e.target;
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+  placeMark(cell, currentClass);
+
+  // check for win
+   //check for draw
+  //switch turns
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass);
+}
+
+// function placeMark(cell, currentClass) {
+//   cell.classsList.add(currentClass);
+// }
