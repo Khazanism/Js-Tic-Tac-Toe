e603fd9400e86867fb097af35e06661119da5452
Khazanins <khazanyassin@gmail.com>
2022-07-13 22:24:44 +0200

    Added Switch Turns Logic + Setting Hover Class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,14 @@
 //  Tic Tac Toe
 
-const X_CLASS = 'x';
-const CIRCLE_CLASS = 'circle';
-
+const X_CLASS = "x";
+const CIRCLE_CLASS = "circle";
 const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.getElementById("board");
 let circleTurn;
 
 cellElements.forEach((cell) => {
   cell.addEventListener("click", handleClick, { once: true });
 });
-
-// function handleClick(e) {
-//   // place mark
-
-//   const cell = e.target;
-//   const currentClass = circleTurn ? CIRCLE_CLASS : x_CLASS;
-//   placeMark(cell, currentClass);
-
-//   // check for win
-//   //check for draw
-//   //switch turns
-// }
 
 function handleClick(e) {
   // place mark
@@ -28,15 +16,28 @@ function handleClick(e) {
   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
   placeMark(cell, currentClass);
 
-  // check for win
-   //check for draw
   //switch turns
+  swapTurns();
+  // Hover mark
+  setBoardHoverClass();
+  // check for win
+  //check for draw
 }
 
 function placeMark(cell, currentClass) {
   cell.classList.add(currentClass);
 }
 
-// function placeMark(cell, currentClass) {
-//   cell.classsList.add(currentClass);
-// }
+function swapTurns() {
+  circleTurn = !circleTurn;
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS);
+  board.classList.remove(CIRCLE_CLASS);
+  if (circleTurn) {
+    board.classList.add(CIRCLE_CLASS);
+  } else {
+    board.classList.add(X_CLASS);
+  }
+}
